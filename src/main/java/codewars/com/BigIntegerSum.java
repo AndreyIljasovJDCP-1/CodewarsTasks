69e522b0b8c28f6eeed631a2e3e33aefb62be105
Andrey Iljasov <a_iljasov@mail.ru>
2023-05-12 12:54:22 +0300

    5kyu: BigInteger Fibonacci sum

--- src/main/java/codewars/com/BigIntegerSum.java
+++ src/main/java/codewars/com/BigIntegerSum.java
@@ -1,6 +1,7 @@
 package codewars.com;
 
 import java.math.BigInteger;
+import java.util.stream.Stream;
 
 /**
  * @see <a href="https://www.codewars.com/kata/60d2325592157c0019ee78ed">
@@ -50,32 +51,21 @@ public class BigIntegerSum {
         var remainderFour = n2.mod(BigInteger.valueOf(4));
         boolean compareToZero = remainderFour.compareTo(BigInteger.ZERO) == 0;
         boolean compareToOne = remainderFour.compareTo(BigInteger.ONE) == 0;
-        switch (lastChar) {
-            case 0:
-                return 0;
-            case 1:
-                return 1;
-            case 2:
-                return odd
-                        ? remainderFour.compareTo(BigInteger.valueOf(3)) == 0 ? 8 : 2
-                        : compareToZero ? 6 : 4;
-            case 3:
-                return odd ? compareToOne ? 3 : 7 : compareToZero ? 1 : 9;
-            case 4:
-                return odd ? 4 : 6;
-            case 5:
-                return 5;
-            case 6:
-                return 6;
-            case 7:
-                return odd ? compareToOne ? 7 : 3 : compareToZero ? 1 : 9;
-            case 8:
-                return odd ? compareToOne ? 8 : 2 : compareToZero ? 6 : 4;
-            case 9:
-                return odd ? 9 : 1;
-            default:
-                return -1;
-        }
+        return switch (lastChar) {
+            case 0 -> 0;
+            case 1 -> 1;
+            case 2 -> odd
+                    ? remainderFour.compareTo(BigInteger.valueOf(3)) == 0 ? 8 : 2
+                    : compareToZero ? 6 : 4;
+            case 3 -> odd ? compareToOne ? 3 : 7 : compareToZero ? 1 : 9;
+            case 4 -> odd ? 4 : 6;
+            case 5 -> 5;
+            case 6 -> 6;
+            case 7 -> odd ? compareToOne ? 7 : 3 : compareToZero ? 1 : 9;
+            case 8 -> odd ? compareToOne ? 8 : 2 : compareToZero ? 6 : 4;
+            case 9 -> odd ? 9 : 1;
+            default -> -1;
+        };
     }
 
     /**
@@ -90,6 +80,40 @@ public class BigIntegerSum {
         BigInteger sumSeq = number.add(BigInteger.ONE).multiply(number).divide(BigInteger.TWO);
         System.out.println(sumSeq);
         return sumSeq.multiply(sumSeq);//?!
+    }
+
+    /**
+     * @see <a href="https://www.codewars.com/kata/559a28007caad2ac4e000083">
+     *     Perimeter of squares in a rectangle</a>
+     * @param n BigInteger
+     * @return perimeter
+     */
+    public static BigInteger perimeter(BigInteger n) {
+
+        return Stream.iterate(
+                        new BigInteger[] {BigInteger.ZERO, BigInteger.ONE},
+                        x -> new BigInteger[] {x[1], x[0].add(x[1])})
+                .limit(n.longValue() + 1)
+                .map(x -> x[1])
+                .reduce(BigInteger.ZERO, BigInteger::add)
+                .multiply(BigInteger.valueOf(4));
+    }
+
+    public static BigInteger perimeterBest(BigInteger n) {
+
+        BigInteger a;
+        BigInteger b = BigInteger.ONE;
+        BigInteger c = BigInteger.ONE;
+        BigInteger sum = BigInteger.ZERO;
+
+        for (int i = 0; i <= n.intValue(); i++) {
+            a = b;
+            b = c;
+            c = a.add(b);
+            sum = sum.add(a);
+        }
+
+        return sum.multiply(BigInteger.valueOf(4));
     }
 
     private static long sumSquares(int n) {
